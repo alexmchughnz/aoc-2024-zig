15754fa9a3082a2ec1856aaf65aa92a15d4b67ff
Alex McHugh <alexmchughnz@icloud.com>
2024-12-02 22:54:49 +1300

    Update template.

--- src/template.zig
+++ src/template.zig
@@ -1,12 +1,24 @@
 const std = @import("std");
 
 const DAY = 1;
+const INPUT = "input.txt";
 
-fn part1() i64 {
+const InputList = std.ArrayList(u8);
+
+fn parse(input_list: *InputList) !void {
+    const file = try std.fs.cwd().readFileAlloc(std.heap.page_allocator, INPUT, std.math.maxInt(usize));
+    var lines = std.mem.splitScalar(u8, file, '\n');
+
+    while (lines.next()) |line| {
+        if (line.len == 0) break;
+    }
+}
+
+fn part1(input: InputList) u64 {
     return 0;
 }
 
-fn part2() i64 {
+fn part2(input: InputList) u64 {
     return 0;
 }
 
@@ -15,9 +27,12 @@ pub fn main() !void {
 
     try stdout.print("\n*** DAY {d} ***\n", .{DAY});
 
-    const answer1 = part1();
+    var input = InputList.init(std.heap.page_allocator);
+    try parse(&input);
+
+    const answer1 = part1(input);
     try stdout.print("Part One = {d}\n", .{answer1});
 
-    const answer2 = part2();
+    const answer2 = part2(input);
     try stdout.print("Part Two = {d}\n", .{answer2});
 }

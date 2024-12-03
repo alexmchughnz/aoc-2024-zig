c26657c292a58f77a449401bb7799bcf65ed1dc9
Alex McHugh <alexmchughnz@icloud.com>
2024-12-03 20:28:56 +1300

    Update template.

--- src/template.zig
+++ src/template.zig
@@ -3,22 +3,21 @@ const std = @import("std");
 const DAY = 1;
 const INPUT = "input.txt";
 
-const InputList = std.ArrayList(u8);
+const LinesList = std.ArrayList([]const u8);
 
-fn parse(input_list: *InputList) !void {
+fn parse(buffer: *LinesList) !void {
     const file = try std.fs.cwd().readFileAlloc(std.heap.page_allocator, INPUT, std.math.maxInt(usize));
-    var lines = std.mem.splitScalar(u8, file, '\n');
-
-    while (lines.next()) |line| {
-        if (line.len == 0) break;
-    }
+    const lines = std.mem.splitScalar(u8, file, '\n');
+    try buffer.append(lines.rest());
 }
 
-fn part1(input: InputList) u64 {
+fn part1(input: LinesList) !u64 {
+    _ = input;
     return 0;
 }
 
-fn part2(input: InputList) u64 {
+fn part2(input: LinesList) !u64 {
+    _ = input;
     return 0;
 }
 
@@ -27,12 +26,12 @@ pub fn main() !void {
 
     try stdout.print("\n*** DAY {d} ***\n", .{DAY});
 
-    var input = InputList.init(std.heap.page_allocator);
+    var input = LinesList.init(std.heap.page_allocator);
     try parse(&input);
 
-    const answer1 = part1(input);
+    const answer1 = try part1(input);
     try stdout.print("Part One = {d}\n", .{answer1});
 
-    const answer2 = part2(input);
+    const answer2 = try part2(input);
     try stdout.print("Part Two = {d}\n", .{answer2});
 }

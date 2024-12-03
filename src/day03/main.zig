0638dcebdf4392eaffe676f16621e365d7459524
Alex McHugh <alexmchughnz@icloud.com>
2024-12-03 21:05:33 +1300

    Solved Day 3-1.

--- src/day03/main.zig
+++ src/day03/main.zig
@@ -3,17 +3,53 @@ const std = @import("std");
 const DAY = 3;
 const INPUT = "input.txt";
 
+const SIZE = 128;
+
+const MUL_TOKEN = "mul";
+
 fn parse() ![]u8 {
     const file = try std.fs.cwd().readFileAlloc(std.heap.page_allocator, INPUT, std.math.maxInt(usize));
     return file;
 }
 
-fn part1(input: []const u8) u64 {
-    std.debug.print("{s}\n", .{input});
-    return 0;
+fn part1(input: []const u8) !u64 {
+    var sum_of_mul_instructions: u64 = 0;
+    var i: usize = 0;
+
+    mul_loop: while (i < input.len) {
+        // Find "mul"...
+        const remaining_input = input[i..];
+        i += std.mem.indexOf(u8, remaining_input, MUL_TOKEN) orelse break;
+
+        // ...followed by '('
+        i += MUL_TOKEN.len;
+        if (input[i] != '(') continue;
+
+        // ...followed by digits until ',' is found
+        i += 1;
+        var n: usize = i;
+        while (input[n] != ',') : (n += 1) {
+            if (!std.ascii.isDigit(input[n])) continue :mul_loop; // Invalid mul instruction.
+        }
+        const num1 = try std.fmt.parseInt(u64, input[i..n], 10);
+
+        // ...followed by digits until ')' is found
+        i = n + 1;
+        var m: usize = i;
+        while (input[m] != ')') : (m += 1) {
+            if (!std.ascii.isDigit(input[m])) continue :mul_loop; // Invalid mul instruction.
+        }
+        const num2 = try std.fmt.parseInt(u64, input[i..m], 10);
+
+        // Valid mul instruction! Increment i and add result to total.
+        i = m + 1;
+        sum_of_mul_instructions += num1 * num2;
+    }
+
+    return sum_of_mul_instructions;
 }
 
-fn part2(input: []const u8) u64 {
+fn part2(input: []const u8) !u64 {
     _ = input;
     return 0;
 }
@@ -25,9 +61,9 @@ pub fn main() !void {
 
     const input: []u8 = try parse();
 
-    const answer1 = part1(input);
+    const answer1 = try part1(input);
     try stdout.print("Part One = {d}\n", .{answer1});
 
-    const answer2 = part2(input);
+    const answer2 = try part2(input);
     try stdout.print("Part Two = {d}\n", .{answer2});
 }

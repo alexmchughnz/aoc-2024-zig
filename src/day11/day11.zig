a7bb1836227f6e80eebaad4136c8a16080da7366
Alex McHugh <alexmchughnz@icloud.com>
2024-12-14 17:23:20 +1300

    Implement blink_result_map.

--- src/day11/day11.zig
+++ src/day11/day11.zig
@@ -5,7 +5,6 @@ const time = std.time;
 const build_options = @import("build_options");
 
 const puzzle_input = @embedFile("input.txt");
-const NUM_BLINKS = 25;
 
 const Stones = std.ArrayList(u64);
 const BlinkResult = [2]?u64;
@@ -29,10 +28,11 @@ fn blink(stone: u64) BlinkResult {
 }
 
 fn part1(input_stones: Stones) u64 {
-    var stones = input_stones;
+    const num_blinks = 25;
+    var stones = input_stones.clone() catch unreachable;
 
-    for (0..NUM_BLINKS) |b| {
-        std.debug.print("Blink {d}...\n", .{b});
+    for (0..num_blinks) |b| {
+        std.debug.print("Blink {d}...\n", .{b + 1});
 
         var index: usize = 0;
         const length = stones.items.len;
@@ -51,9 +51,35 @@ fn part1(input_stones: Stones) u64 {
     return stones.items.len;
 }
 
-fn part2(stones: Stones) u64 {
-    _ = stones;
-    return 0;
+fn part2(input_stones: Stones) u64 {
+    const num_blinks = 75;
+    var stones = input_stones.clone() catch unreachable;
+
+    var blink_result_map = std.AutoHashMap(u64, BlinkResult).init(std.heap.page_allocator);
+
+    for (0..num_blinks) |b| {
+        std.debug.print("Blink {d}...\n", .{b + 1});
+
+        var index: usize = 0;
+        const length = stones.items.len;
+
+        while (index < length) : (index += 1) {
+            const stone: *u64 = &stones.items[index];
+
+            const blink_result: *BlinkResult = blk: {
+                const res = blink_result_map.getOrPut(stone.*) catch unreachable;
+                if (!res.found_existing) res.value_ptr.* = blink(stone.*);
+                break :blk res.value_ptr;
+            };
+
+            stone.* = blink_result[0] orelse unreachable;
+            if (blink_result[1]) |new_stone| {
+                stones.append(new_stone) catch unreachable;
+            }
+        }
+    }
+
+    return stones.items.len;
 }
 
 fn parse(buffer: *Stones) !void {

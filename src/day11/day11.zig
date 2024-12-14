86e896000eceae68974a4d437b62e1e6577bd083
Alex McHugh <alexmchughnz@icloud.com>
2024-12-15 07:17:17 +1300

    Solved Day 11-2.

--- src/day11/day11.zig
+++ src/day11/day11.zig
@@ -7,6 +7,7 @@ const build_options = @import("build_options");
 const puzzle_input = @embedFile("input.txt");
 
 const Stones = std.ArrayList(u64);
+const StoneFreq = struct { stone: u64, freq: u64 };
 const BlinkResult = [2]?u64;
 
 fn blink(stone: u64) BlinkResult {
@@ -53,33 +54,56 @@ fn part1(input_stones: Stones) u64 {
 
 fn part2(input_stones: Stones) u64 {
     const num_blinks = 75;
-    var stones = input_stones.clone() catch unreachable;
 
     var blink_result_map = std.AutoHashMap(u64, BlinkResult).init(std.heap.page_allocator);
+
+    var stone_freq_map = std.AutoHashMap(u64, u64).init(std.heap.page_allocator);
+    for (input_stones.items) |stone| {
+        const res = stone_freq_map.getOrPutValue(stone, 0) catch unreachable;
+        res.value_ptr.* = 1;
+    }
 
     for (0..num_blinks) |b| {
         std.debug.print("Blink {d}...\n", .{b + 1});
 
-        var index: usize = 0;
-        const length = stones.items.len;
+        var additions = std.ArrayList(StoneFreq).init(std.heap.page_allocator);
+        defer additions.clearAndFree();
 
-        while (index < length) : (index += 1) {
-            const stone: *u64 = &stones.items[index];
+        var it = stone_freq_map.iterator();
+        while (it.next()) |entry| {
+            const stone = entry.key_ptr.*;
+            const freq = entry.value_ptr.*;
 
+            // Clear stone count from map.
+            entry.value_ptr.* = 0;
+
+            // Get blink result for stone value.
             const blink_result: *BlinkResult = blk: {
-                const res = blink_result_map.getOrPut(stone.*) catch unreachable;
-                if (!res.found_existing) res.value_ptr.* = blink(stone.*);
+                const res = blink_result_map.getOrPut(stone) catch unreachable;
+                if (!res.found_existing) res.value_ptr.* = blink(stone);
                 break :blk res.value_ptr;
             };
 
-            stone.* = blink_result[0] orelse unreachable;
-            if (blink_result[1]) |new_stone| {
-                stones.append(new_stone) catch unreachable;
+            // Add each new stone, with frequency of the input stone, to additions list.
+            for (blink_result) |n| {
+                if (n) |new_stone| {
+                    const addition = StoneFreq{ .stone = new_stone, .freq = freq };
+                    additions.append(addition) catch unreachable;
+                }
             }
+        }
+
+        // Add additions list into map for next blink.
+        for (additions.items) |sf| {
+            const res = stone_freq_map.getOrPutValue(sf.stone, 0) catch unreachable;
+            res.value_ptr.* += sf.freq;
         }
     }
 
-    return stones.items.len;
+    var totalFreq: u64 = 0;
+    var it = stone_freq_map.valueIterator();
+    while (it.next()) |freq| totalFreq += freq.*;
+    return totalFreq;
 }
 
 fn parse(buffer: *Stones) !void {

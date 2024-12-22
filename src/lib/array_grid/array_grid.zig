4a493b218b46b5b6e128653e6ed5d979f1000930
Alex McHugh <alexmchughnz@icloud.com>
2024-12-23 06:49:47 +1300

    array_grid: Refactor module.

--- src/lib/array_grid/array_grid.zig
+++ src/lib/array_grid/array_grid.zig
@@ -1,78 +1,11 @@
 const std = @import("std");
 const expect = std.testing.expect;
 
-pub const GridError = error{OutOfBounds};
+const grid_index = @import("grid_index.zig");
+const GridIndex = grid_index.GridIndex;
+const GridDirection = grid_index.GridDirection;
 
-pub const GridIndex = struct {
-    x: isize = 0,
-    y: isize = 0,
-
-    fn add(self: GridIndex, addend: GridIndex) GridIndex {
-        return GridIndex{ .x = self.x + addend.x, .y = self.y + addend.y };
-    }
-
-    fn move(self: *GridIndex, dir: GridDirection) void {
-        self.* = self.add(dir.toIndex());
-    }
-};
-
-test "GridIndex.add" {
-    const index = GridIndex{ .x = 2, .y = 2 };
-    var result = GridIndex{};
-
-    result = index.add(.{ .x = 1, .y = 2 });
-    try expect(result.x == 3 and result.y == 4);
-    result = index.add(.{ .x = -2, .y = -2 });
-    try expect(result.x == 0 and result.y == 0);
-    result = index.add(.{ .x = -4, .y = -4 });
-    try expect(result.x == -2 and result.y == -2);
-}
-
-test "GridIndex.move" {
-    var index = GridIndex{};
-
-    index.move(GridDirection.Down);
-    try expect(index.x == 0 and index.y == 1);
-    index.move(GridDirection.Right);
-    try expect(index.x == 1 and index.y == 1);
-    index.move(GridDirection.Up);
-    try expect(index.x == 1 and index.y == 0);
-    index.move(GridDirection.Left);
-    try expect(index.x == 0 and index.y == 0);
-
-    index.move(GridDirection.DownRight);
-    try expect(index.x == 1 and index.y == 1);
-    index.move(GridDirection.DownLeft);
-    try expect(index.x == 0 and index.y == 2);
-    index.move(GridDirection.UpLeft);
-    try expect(index.x == -1 and index.y == 1);
-    index.move(GridDirection.UpRight);
-    try expect(index.x == 0 and index.y == 0);
-}
-
-pub const GridDirection = enum {
-    Up,
-    Down,
-    Left,
-    Right,
-    UpLeft,
-    UpRight,
-    DownLeft,
-    DownRight,
-
-    fn toIndex(self: GridDirection) GridIndex {
-        return switch (self) {
-            GridDirection.Up => .{ .x = 0, .y = -1 },
-            GridDirection.Down => .{ .x = 0, .y = 1 },
-            GridDirection.Left => .{ .x = -1, .y = 0 },
-            GridDirection.Right => .{ .x = 1, .y = 0 },
-            GridDirection.UpLeft => .{ .x = -1, .y = -1 },
-            GridDirection.UpRight => .{ .x = 1, .y = -1 },
-            GridDirection.DownLeft => .{ .x = -1, .y = 1 },
-            GridDirection.DownRight => .{ .x = 1, .y = 1 },
-        };
-    }
-};
+pub const ArrayGridError = error{OutOfBounds};
 
 pub fn ArrayGrid(T: type) type {
     return struct {
@@ -100,8 +33,8 @@ pub fn ArrayGrid(T: type) type {
             return contains_x and contains_y;
         }
 
-        pub fn at(self: ArrayGrid(T), index: GridIndex) GridError!T {
-            if (!self.contains(index)) return GridError.OutOfBounds;
+        pub fn at(self: ArrayGrid(T), index: GridIndex) ArrayGridError!T {
+            if (!self.contains(index)) return ArrayGridError.OutOfBounds;
 
             const x: usize = @intCast(index.x);
             const y: usize = @intCast(index.y);

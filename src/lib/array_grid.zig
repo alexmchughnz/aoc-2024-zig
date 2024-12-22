222b122275c61f185460c0e060806f7e0422f575
Alex McHugh <alexmchughnz@icloud.com>
2024-12-23 06:38:39 +1300

    array_grid: Add GridIndex.move.

--- src/lib/array_grid.zig
+++ src/lib/array_grid.zig
@@ -4,13 +4,51 @@ const expect = std.testing.expect;
 pub const GridError = error{OutOfBounds};
 
 pub const GridIndex = struct {
-    x: isize,
-    y: isize,
+    x: isize = 0,
+    y: isize = 0,
 
     fn add(self: GridIndex, addend: GridIndex) GridIndex {
         return GridIndex{ .x = self.x + addend.x, .y = self.y + addend.y };
     }
+
+    fn move(self: *GridIndex, dir: GridDirection) void {
+        self.* = self.add(dir.toIndex());
+    }
 };
+
+test "GridIndex.add" {
+    const index = GridIndex{ .x = 2, .y = 2 };
+    var result = GridIndex{};
+
+    result = index.add(.{ .x = 1, .y = 2 });
+    try expect(result.x == 3 and result.y == 4);
+    result = index.add(.{ .x = -2, .y = -2 });
+    try expect(result.x == 0 and result.y == 0);
+    result = index.add(.{ .x = -4, .y = -4 });
+    try expect(result.x == -2 and result.y == -2);
+}
+
+test "GridIndex.move" {
+    var index = GridIndex{};
+
+    index.move(GridDirection.Down);
+    try expect(index.x == 0 and index.y == 1);
+    index.move(GridDirection.Right);
+    try expect(index.x == 1 and index.y == 1);
+    index.move(GridDirection.Up);
+    try expect(index.x == 1 and index.y == 0);
+    index.move(GridDirection.Left);
+    try expect(index.x == 0 and index.y == 0);
+
+    index.move(GridDirection.DownRight);
+    try expect(index.x == 1 and index.y == 1);
+    index.move(GridDirection.DownLeft);
+    try expect(index.x == 0 and index.y == 2);
+    index.move(GridDirection.UpLeft);
+    try expect(index.x == -1 and index.y == 1);
+    index.move(GridDirection.UpRight);
+    try expect(index.x == 0 and index.y == 0);
+}
 
 pub const GridDirection = enum {
     Up,
@@ -22,7 +60,7 @@ pub const GridDirection = enum {
     DownLeft,
     DownRight,
 
-    fn getDeltaIndex(self: GridDirection) GridIndex {
+    fn toIndex(self: GridDirection) GridIndex {
         return switch (self) {
             GridDirection.Up => .{ .x = 0, .y = -1 },
             GridDirection.Down => .{ .x = 0, .y = 1 },
@@ -31,7 +69,7 @@ pub const GridDirection = enum {
             GridDirection.UpLeft => .{ .x = -1, .y = -1 },
             GridDirection.UpRight => .{ .x = 1, .y = -1 },
             GridDirection.DownLeft => .{ .x = -1, .y = 1 },
-            GridDirection.DownRight => .{ .x = 1, .y = -1 },
+            GridDirection.DownRight => .{ .x = 1, .y = 1 },
         };
     }
 };
